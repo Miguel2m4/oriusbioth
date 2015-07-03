ce9718643be6ee1a6711f4158f0c8438a150ab9a
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-07-02 21:40:39 -0500

    inicio de la web

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width , initial-scale=1 ,maximum-scale=1 user-scalable=no" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" >
+<meta name="keywords" lang="es" content="">
+<meta name="robots" content="All">
+<meta name="description" lang="es" content="">
+<title>Orius Biotech | Soluciones para la producción agropecuaria sostenible</title>
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/stylesheet.css" />
+</head>
+<body>
+
+</body>
+</html>
