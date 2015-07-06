5c888800ee754c5bb758300f502718db5a3fb355
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-07-06 09:59:52 -0500

    header

--- index.php
+++ index.php
@@ -3,15 +3,64 @@
 <head>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width , initial-scale=1 ,maximum-scale=1 user-scalable=no" />
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" >
-<meta name="keywords" lang="es" content="">
+<!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" > -->
+<meta name="keywords" lang="es" content="bioinsumos, biotecnología, bioplaguicidas, plaguicidas biológicos, productos biológicos, producción agrícola, producción pecuaria, producción acuícola, problemas ambientales, agricultura orgánica, buenas prácticas agrícolas, alimentos sanos, agroexportadores, café especial, bionutrición, PGPR, producción orgánica, aguas residuales, abono orgánico, suelo sano - cultivo sano, pozos sépticos, estanques de oxidación,  sostenibilidad, producción sostenible, menor impacto ambiental, agricultura, ganaderia, banano, arroz, café, flores, invernaderos, producción tecnificada, agricultura de precisión">
 <meta name="robots" content="All">
-<meta name="description" lang="es" content="">
+<meta name="description" lang="es" content="Soluciones con Biotecnología! ORIUS BIOTECH es una organización de base tecnológica que investiga, produce, desarrolla,  comercializa y exporta soluciones con biotecnología para los productores agrícolas, pecuarios, acuícolas y para el manejo ambiental, para lograr una producción con menor impacto ambiental y sostenible.">
 <title>Orius Biotech | Soluciones para la producción agropecuaria sostenible</title>
 <link rel="stylesheet" href="css/normalize.css" />
 <link rel="stylesheet" href="css/stylesheet.css" />
 </head>
 <body>
+<header>
+<div class="distribuidores">
+	<div class="distri-bandera">
+		<img src="images/bdcolombia.png">
+		<a href="#">Colombia</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdchile.png">
+		<a href="#">Chile</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdcosta.png">
+		<a href="#">Costa Rica</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdecuador.png">
+		<a href="#">Ecuador</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdpanama.png">
+		<a href="#">Panamá</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdperu.png">
+		<a href="#">Perú</a>
+	</div>
+</div>
+<div class="top">
+	<div class="top-izq">
+	<img src="images/logo.png">
+	</div>
+	<div class="top-der">
+	<nav>
+		<ul>
+			<li><a href="#nowhere">Productos Soluciones</a></li>
+			<li><a href="#nowhere">Catalogo</a></li>
+			<li><a href="#nowhere">Escritos Técnicos</a></li>
+			<li><a href="#nowhere">Resultados Investigación Aplicada</a></li>
+		</ul>
+	</nav>
+	</div>
+</div>
+</header>
+<section>
+
+</section>
+<footer>
+
+</footer>
 
 </body>
 </html>
