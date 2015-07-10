3a7a2da2639f82f25d9651df1dc8cc0b1785a793
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-07-10 18:43:04 -0500

    web oriusbiotech

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="pie">
+			<div class="redes">
+				<p>Redes sociales</p>
+			</div>
+			<hr class="divi2">
+			<div class="copyright">
+            <p>Copyright © Oriusbiotech SAS, 2015.Todos los derechos reservados diseñado por <a href="http://inngeniate.com/" target="blank"> Inngeniate.com</a></p>
+            <div class="logo">
+                <img src="images/logoinn.png">
+            </div>
+        	</div>
+</div>
--- index.php
+++ index.php
@@ -133,19 +133,7 @@
 	</div>
 </section>
 <footer>
-	<div class="pie">
-			<div class="redes">
-				<p>Redes sociales</p>
-			</div>
-			<hr class="divi2">
-			<div class="copyright">
-            <p>Copyright © Oriusbiotech SAS, 2015.Todos los derechos reservados diseñado por <a href="http://inngeniate.com/" target="blank"> Inngeniate.com</a></p>
-            <div class="logo">
-                <img src="images/logoinn.png">
-            </div>
-        	</div>
-	</div>
-
+	<?php include("footer.php"); ?>
 </footer>
 
 </body>
