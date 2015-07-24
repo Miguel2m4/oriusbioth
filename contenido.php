b63f68a200a9fa4f686260db8abe2b338a520ec4
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-07-23 22:46:29 -0500

    web oriusbiotech

---
+++ contenido.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width , initial-scale=1 ,maximum-scale=1 user-scalable=no" />
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" >
+<meta name="keywords" lang="es" content="bioinsumos, biotecnología, bioplaguicidas, plaguicidas biológicos, productos biológicos, producción agrícola, producción pecuaria, producción acuícola, problemas ambientales, agricultura orgánica, buenas prácticas agrícolas, alimentos sanos, agroexportadores, café especial, bionutrición, PGPR, producción orgánica, aguas residuales, abono orgánico, suelo sano - cultivo sano, pozos sépticos, estanques de oxidación,  sostenibilidad, producción sostenible, menor impacto ambiental, agricultura, ganaderia, banano, arroz, café, flores, invernaderos, producción tecnificada, agricultura de precisión">
+<meta name="robots" content="All">
+<meta name="description" lang="es" content="Soluciones con Biotecnología! ORIUS BIOTECH es una organización de base tecnológica que investiga, produce, desarrolla,  comercializa y exporta soluciones con biotecnología para los productores agrícolas, pecuarios, acuícolas y para el manejo ambiental, para lograr una producción con menor impacto ambiental y sostenible.">
+<title>Contenido | Orius Biotech | Soluciones para la producción agropecuaria sostenible</title>
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/stylesheet.css" />
+<link rel="stylesheet" type="text/css" href="css/style-menu.css">
+<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+</head>
+<body>
+<header>
+<div class="distribuidores">
+	<div class="distri-bandera">
+		<img src="images/bdcolombia.png">
+		<a href="#">Colombia</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdchile.png">
+		<a href="#">Chile</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdcosta.png">
+		<a href="#">Costa Rica</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdecuador.png">
+		<a href="#">Ecuador</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdpanama.png">
+		<a href="#">Panamá</a>
+	</div>
+	<div class="distri-bandera">
+		<img src="images/bdperu.png">
+		<a href="#">Perú</a>
+	</div>
+</div>
+<div class="distribuidores2">
+	<ul>
+		<li><a href="">Colombia</a></li>
+		<li><a href="">Chile</a></li>
+		<li><a href="">Costa Rica</a></li>
+		<li><a href="">Ecuador</a></li>
+		<li><a href="">Perú</a></li>
+	</ul>
+</div>
+<div class="top">
+	<div class="top-izq">
+	<a href="/"><img src="images/logo.png"></a>
+	</div>
+	<div class="top-der">
+	<?php include("menu.php"); ?>
+	</div>
+	<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="js/script-menu.js"></script>
+</div>
+</header>
+<section>
+	<div class="imagen-fondo-blog">
+		<div class="imagen-titulo-blog">
+			<h2>Titulo del blog probando el tamaño del nombre sin qeu se dañe el diseño</h2>
+		</div>
+	</div>
+	<!-- <div class="tooper"></div> -->
+	<div class="mashead">
+	<h2>Espigador</h2>
+	</div>
+	<div class="contenido">
+	<div class="soluciones">
+	<img src="images/espigador.png">
+	<h3>Granos con mas Peso y mas Calidad!</h3>
+	<p>ESPIGADOR es un Fertilizante Orgánico Mineral formulado con minerales quelatados en aminoácidos de rápida asimilación que aplicado en prefloración y floración, que activa la energía y el metabolismo en las plantas mejorando el balance nutricional en los cultivos de Gramíneas como Arroz, Sorgo, Cebada, Avena, Trigo y Maíz, disminuyendo el vaneamiento de la espiga, logrando un máximo llenado del grano, mayor ganancia en peso del grano y una muy buena calidad final en la molinería. Estos quelatos que se obtienen por acción microbiana y enzimática son de rápida asimilación por la planta, le aportan la energía y los nutrientes necesarios en forma inmediata para activar los cultivos, recuperarlos y darle el balance nutricional para mejorar la capacidad productiva del cultivo.</p>
+
+
+	<p><a href="">Comprar Espigador SC Litro</a></p>
+
+	<p><strong>Documentos Técnicos</strong></p>
+	<p><a href="" target="blank"><span class="icon-descarga"></span> ESPIGADOR SC - FICHA TECNICA</a></p>
+
+	<p><strong>Publicidad</strong></p>
+	<p><a href="" target="blank"><span class="icon-descarga"></span> ESPIGADOR - FOLLETO</a></p>
+
+	</div>
+
+
+
+	</div>
+
+
+
+
+	<div class="seccion-contacto">
+		<p>Tienen alguna duda acerca de nuestras soluciones?</p><a href="">Contáctenos</a>
+	</div>
+</section>
+<footer>
+	<?php include("footer.php"); ?>
+</footer>
+<script src="js/mostrar-invest.js"></script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.4";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</body>
+</html>
