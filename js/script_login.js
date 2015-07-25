910c41f2f41c41a6050b8f44f617365d4dc8c9cd
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-07-24 20:29:14 -0500

    web oriusbiotech

--- js/script_login.js
+++ js/script_login.js
@@ -19,39 +19,39 @@ $(document).ready(function(){
 			url: 'libs/logueo.php',
 			success: function(data)
 			{
-				// if(data == 'error')
-				// 	{
-		  //   			$('#fondo').remove();
-				// 		$('body').append("<div class='fondo' id='fondo' style='display:none;'></div>");
-				// 		$('#fondo').append("<div class='caja' style='display: none; text-align: center; background: rgb(238, 92, 92)' id='rp'><span>El usuario y/o contraseña no son correctos</span></div>");
-				// 		setTimeout(function() {
-				//         	$('#fondo').fadeIn('fast',function(){
-				//             $('#rp').animate({'top':'350px'},50).fadeIn();
-				//          	});
-				//         }, 400);
-				//         setTimeout(function() {
-				//             $("#rp").fadeOut();
-				//             $('#fondo').fadeOut('fast');
-				//         }, 2000);
-				// 	}
-				// else
-				// 	{
-		  //   			$('#fondo').remove();
-				// 		$('body').append("<div class='fondo' id='fondo' style='display:none;'></div>");
-				// 		$('#fondo').append("<div class='caja' style='display: none; text-align: center;' id='rp'><span>Ingresando. . . </span></div>");
-				// 		setTimeout(function() {
-				//         	$('#fondo').fadeIn('fast',function(){
-				//             $('#rp').animate({'top':'350px'},50).fadeIn();
-				//          	});
-				//         }, 400);
-				//         setTimeout(function() {
-				//             $("#rp").fadeOut();
-				//             $('#fondo').fadeOut('fast');
-				//         }, 2000);
-				// 		setTimeout(function() {
-				// 		window.location.href = data['redirec'];
-				// 		}, 2500);
-				// 	}
+				if(data == 'error')
+					{
+		    			$('#fondo').remove();
+						$('body').append("<div class='fondo' id='fondo' style='display:none;'></div>");
+						$('#fondo').append("<div class='caja' style='display: none; text-align: center; background: rgb(238, 92, 92)' id='rp'><span>El usuario y/o contraseña no son correctos</span></div>");
+						setTimeout(function() {
+				        	$('#fondo').fadeIn('fast',function(){
+				            $('#rp').animate({'top':'350px'},50).fadeIn();
+				         	});
+				        }, 400);
+				        setTimeout(function() {
+				            $("#rp").fadeOut();
+				            $('#fondo').fadeOut('fast');
+				        }, 2000);
+					}
+				else
+					{
+		    			$('#fondo').remove();
+						$('body').append("<div class='fondo' id='fondo' style='display:none;'></div>");
+						$('#fondo').append("<div class='caja' style='display: none; text-align: center;' id='rp'><span>Ingresando. . . </span></div>");
+						setTimeout(function() {
+				        	$('#fondo').fadeIn('fast',function(){
+				            $('#rp').animate({'top':'350px'},50).fadeIn();
+				         	});
+				        }, 400);
+				        setTimeout(function() {
+				            $("#rp").fadeOut();
+				            $('#fondo').fadeOut('fast');
+				        }, 2000);
+						setTimeout(function() {
+						window.location.href = data['redirec'];
+						}, 2500);
+					}
 			}
 		});
 	});
