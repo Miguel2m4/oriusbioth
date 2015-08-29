260eb8c86b07cc36659d732fa01ea84752bc20cf
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-08-28 19:51:43 -0500

    web oriusbiotech

--- catalogo/libs/conexion.php
+++ catalogo/libs/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion=mysql_connect("127.0.0.1","root","")or die ("No hay Conexion");
-$conectDB=mysql_select_db("sig",$conexion) or die ("no existe BD");
+$conexion=mysql_connect("localhost","oriuspan_Sig2014","Sigapp14")or die ("No hay Conexion");
+$conectDB=mysql_select_db("oriuspan_sigapp2",$conexion) or die ("no existe BD");
 mysql_query ("SET NAMES 'utf8'");
 ?>
